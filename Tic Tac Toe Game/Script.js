c16eb65927a831495aae384b65abc48501670da5
jtaljaard34 <148882704+jtaljaard34@users.noreply.github.com>
2024-03-13 15:45:02 +0200

    commit

---
+++ Tic Tac Toe Game/Script.js
@@ -0,0 +1,6 @@
+let btnRef =document.querySelectorAll(".button-option");
+let popupRef =document.querySelector(".popup");
+let newGameBtn = document.getElementById("new-game");
+let restartBtn = document.getElementById("restart");
+
+
